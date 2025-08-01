3c6e99a1a9f32756e62e0529aa9c5e43a51ecb83
kymjaehong <dwby.jr@gmail.com>
2025-08-01 23:43:50 +0900

    feat: 이동 히스토리 저장하기 #9

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@
 import { useState } from 'react';
 import './App.css'
 
-export default function Board() {
+export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+  
+  function handlePlay(nextSquares) {
+    // console.log("BEFORE: ", currentSquares);
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
 
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -18,8 +38,8 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
+    // console.log("AFTER: ", nextSquares);
   }
 
   const winner = calculateWinner(squares);

1e0cabfef94709ab0a6e25260e5f545b0802ca5b
kymjaehong <dwby.jr@gmail.com>
2025-08-01 22:18:52 +0900

    edit: 사용자와 상호작용하는 컴포넌트 #4

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 // App.js의 코드는 컴포넌트를 생성합니다. 
 
+import { useState } from 'react';
 import './App.css'
 
 export default function Board() {
    return (
     <>
       <div className='board-row'>
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='board-row'>
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='board-row'>
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
 }
 
-function Square({ value }) {
+function Square() {
+  const [value, setValue] = useState(null);
+
   function handleClick() {
-    alert("clicked!");
+    setValue('X');
   }
 
   return (

989262a2b54c30b3712731a024c69a6226a6a092
kymjaehong <dwby.jr@gmail.com>
2025-08-02 00:06:27 +0900

    feat: 시간여행 구현하기 #11

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import './App.css'
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
   
   function handlePlay(nextSquares) {
-    // console.log("BEFORE: ", currentSquares);
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
   
   function jumpTo(nextIndex) {
-    // TODO
+    setCurrentMove(nextIndex);
+    setXIsNext(nextIndex % 2 === 0);
   }
 
   const moves = history.map((squares, index) => {

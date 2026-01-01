f1b828f867a3633904d3f883dea316c12388bb18
kymjaehong <dwby.jr@gmail.com>
2026-01-02 00:08:13 +0900

    feat: json-server로 간단 백엔드 구축

--- onebite-exam/vite.config.ts
+++ onebite-exam/vite.config.ts
@@ -1,7 +1,7 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +11,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    watch: {
+      // vite가 server 파일 아래의 모든 파일을 감지하지 않음
+      // 해당 파일에 변화가 생겨도 리액트 앱이 리렌더링되지 않음
+      ignored: ["**/server/**"],
+    },
+  },
+});

205f312a5b85d42aa89c1af6d06a118f5b1cb993
kymjaehong <dwby.jr@gmail.com>
2026-01-04 00:51:52 +0900

    fix: 데이터 조회 시 캐시 데이터 정규화

--- onebite-exam/src/hooks/queries/use-todo-data-by-id.ts
+++ onebite-exam/src/hooks/queries/use-todo-data-by-id.ts
@@ -2,13 +2,16 @@ import { fetchTodoById } from "@/api/fetch-todo-by-id";
 import { QUERY_KEYS } from "@/lib/constants";
 import { useQuery } from "@tanstack/react-query";
 
-export function useTodoDataById(id: string) {
+export function useTodoDataById(id: string, type: "LIST" | "DETAIL") {
   return useQuery({
     queryFn: () => fetchTodoById(id),
     queryKey: QUERY_KEYS.todo.detail(id),
+    // 해당 경우에만 refetching
+    // 다른 경우에는 캐시 데이터가 disabled로 설정됨.
+    enabled: type === "DETAIL",
 
-    staleTime: 5000, // 5000ms동안 fresh 유지
-    gcTime: 5000, // 5000ms 이후 캐시 데이터 삭제
+    // staleTime: 5000, // 5000ms동안 fresh 유지
+    // gcTime: 5000, // 5000ms 이후 캐시 데이터 삭제
 
     // refetchInterval: 1000, // 1000ms마다 api 호출
   });
--- onebite-exam/src/hooks/queries/use-todos-data.ts
+++ onebite-exam/src/hooks/queries/use-todos-data.ts
@@ -1,10 +1,23 @@
 import { fetchTodos } from "@/api/fetch-todos";
 import { QUERY_KEYS } from "@/lib/constants";
-import { useQuery } from "@tanstack/react-query";
+import type { Todo } from "@/types";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 export function useTodosData() {
+  const queryClient = useQueryClient();
+
   return useQuery({
-    queryFn: fetchTodos,
+    queryFn: async () => {
+      // 리턴 값을 캐시 데이터로 저장하기 때문에 id 값만 저장합니다.
+      const todos = await fetchTodos();
+
+      // 개별 캐시 데이터로 저장
+      todos.forEach((todo) => {
+        queryClient.setQueryData<Todo>(QUERY_KEYS.todo.detail(todo.id), todo);
+      });
+
+      return todos.map((todo) => todo.id);
+    },
     queryKey: QUERY_KEYS.todo.list,
     // retry: 0,
   });

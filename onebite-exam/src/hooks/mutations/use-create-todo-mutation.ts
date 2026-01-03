dd46e7059627f1862011866b6a6f8376d277495b
kymjaehong <dwby.jr@gmail.com>
2026-01-03 22:44:16 +0900

    fix: 캐시 데이터 무효화, 데이터 리패칭

--- onebite-exam/src/hooks/mutations/use-create-todo-mutation.ts
+++ onebite-exam/src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,13 +1,22 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createTodo } from "@/api/create-todo";
+import { useTodosData } from "../queries/use-todos-data";
 
 export function useCreateTodoMutation() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: createTodo,
     onMutate: () => {},
     onSettled: () => {},
     onSuccess: () => {
-      window.location.reload(); // 안 좋은 방법
+      //   window.location.reload(); // 안 좋은 방법
+
+      // 좋은 방법
+      // "todos" 키를 가진 캐시 데이터를 무효화
+      queryClient.invalidateQueries({
+        queryKey: ["todos"],
+      });
     },
     onError: (error) => {
       window.alert(error.message);

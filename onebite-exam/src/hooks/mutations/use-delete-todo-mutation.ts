1a79ed04eda05c7582a31e5050b200e014265fb2
kymjaehong <dwby.jr@gmail.com>
2026-01-04 01:04:33 +0900

    fix: 데이터 삭제 시 캐시 데이터 정규화

--- onebite-exam/src/hooks/mutations/use-delete-todo-mutation.ts
+++ onebite-exam/src/hooks/mutations/use-delete-todo-mutation.ts
@@ -1,6 +1,5 @@
 import { deleteTodo } from "@/api/delete-todo";
 import { QUERY_KEYS } from "@/lib/constants";
-import type { Todo } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeleteTodoMutation() {
@@ -9,10 +8,22 @@ export function useDeleteTodoMutation() {
   return useMutation({
     mutationFn: deleteTodo,
     onSuccess: (deletdTodo) => {
-      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
-        if (!prevTodos) return [];
-        return prevTodos.filter((prevTodo) => prevTodo.id !== deletdTodo.id);
+      // queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+      //   if (!prevTodos) return [];
+      //   return prevTodos.filter((prevTodo) => prevTodo.id !== deletdTodo.id);
+      // });
+
+      // 데이터 정규화 (개선)
+      queryClient.removeQueries({
+        queryKey: QUERY_KEYS.todo.detail(deletdTodo.id),
       });
+      queryClient.setQueryData<string[]>(
+        QUERY_KEYS.todo.list,
+        (prevTodoIds) => {
+          if (!prevTodoIds) return [];
+          return prevTodoIds.filter((id) => id !== deletdTodo.id);
+        },
+      );
     },
   });
 }

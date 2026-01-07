7bc4d74a881d1ad5afccc63f14a79445a46577b5
kymjaehong <dwby.jr@gmail.com>
2026-01-08 00:28:05 +0900

    fix: 세션 관리 로직을 provider로 분리 (#12)

--- onebite-log/src/App.tsx
+++ onebite-log/src/App.tsx
@@ -1,21 +1,10 @@
 import RootRoute from "@/root-route";
-import supabase from "@/lib/supabase";
-import { useEffect } from "react";
-import { useIsSessionLoaded, useSetSession } from "./store/session";
+import SessionProvider from "./provider/session-provider";
 
 export default function App() {
-  const setSession = useSetSession();
-  const isSessionLoaded = useIsSessionLoaded();
-
-  useEffect(() => {
-    supabase.auth.onAuthStateChange((event, session) => {
-      setSession(session);
-    });
-  }, []);
-
-  // 세션 데이터가 로딩 중일 때 화면 고정
-  // 화면이 전환되거나 튕기는 이슈 방지
-  if (!isSessionLoaded) return <div>로딩 중...</div>;
-
-  return <RootRoute />;
+  return (
+    <SessionProvider>
+      <RootRoute />
+    </SessionProvider>
+  );
 }
---
+++ onebite-log/src/provider/session-provider.tsx
@@ -0,0 +1,20 @@
+import supabase from "@/lib/supabase";
+import { useIsSessionLoaded, useSetSession } from "@/store/session";
+import { useEffect, type ReactNode } from "react";
+
+export default function SessionProvider({ children }: { children: ReactNode }) {
+  const setSession = useSetSession();
+  const isSessionLoaded = useIsSessionLoaded();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  // 세션 데이터가 로딩 중일 때 화면 고정
+  // 화면이 전환되거나 튕기는 이슈 방지
+  if (!isSessionLoaded) return <div>로딩 중...</div>;
+
+  return children;
+}

b1f60e11fa6a7db150cedcdfd4c55ed2c23e768d
kymjaehong <dwby.jr@gmail.com>
2026-01-08 00:18:13 +0900

    feat: zustand store에 세션 관리 및 app.tsx 마운트 시점에 session 저장(#12)

--- onebite-log/src/App.tsx
+++ onebite-log/src/App.tsx
@@ -1,5 +1,21 @@
-import RootRoute from "./root-route";
+import RootRoute from "@/root-route";
+import supabase from "@/lib/supabase";
+import { useEffect } from "react";
+import { useIsSessionLoaded, useSetSession } from "./store/session";
 
 export default function App() {
+  const setSession = useSetSession();
+  const isSessionLoaded = useIsSessionLoaded();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  // 세션 데이터가 로딩 중일 때 화면 고정
+  // 화면이 전환되거나 튕기는 이슈 방지
+  if (!isSessionLoaded) return <div>로딩 중...</div>;
+
   return <RootRoute />;
 }

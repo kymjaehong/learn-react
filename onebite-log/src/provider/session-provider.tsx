da781d24a8511d1a543b3477bb670f0e367fada3
kymjaehong <dwby.jr@gmail.com>
2026-01-13 00:50:59 +0900

    feat: 회원가입시 프로필 정보 자동 생성 (#12)

--- onebite-log/src/provider/session-provider.tsx
+++ onebite-log/src/provider/session-provider.tsx
@@ -1,11 +1,17 @@
 import GlobalLoader from "@/components/global-loader";
+import { useProfileData } from "@/hooks/queries/use-profile-data";
 import supabase from "@/lib/supabase";
-import { useIsSessionLoaded, useSetSession } from "@/store/session";
+import { useIsSessionLoaded, useSession, useSetSession } from "@/store/session";
 import { useEffect, type ReactNode } from "react";
 
 export default function SessionProvider({ children }: { children: ReactNode }) {
+  const session = useSession();
   const setSession = useSetSession();
   const isSessionLoaded = useIsSessionLoaded();
+
+  const { data: profile, isLoading: isProfileLoading } = useProfileData(
+    session?.user.id,
+  );
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
@@ -16,6 +22,7 @@ export default function SessionProvider({ children }: { children: ReactNode }) {
   // 세션 데이터가 로딩 중일 때 화면 고정
   // 화면이 전환되거나 튕기는 이슈 방지
   if (!isSessionLoaded) return <GlobalLoader />;
+  if (isProfileLoading) return <GlobalLoader />;
 
   return children;
 }
--- onebite-log/src/types.ts
+++ onebite-log/src/types.ts
@@ -1,6 +1,7 @@
 import { type Database } from "./databse.types";
 
 export type PostEntity = Database["public"]["Tables"]["post"]["Row"];
+export type ProfileEntity = Database["public"]["Tables"]["profile"]["Row"];
 
 export type useMutationCallback = {
   onSuccess?: () => void;

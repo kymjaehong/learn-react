d46abff962f876c38ba60b64d87a0fede5444c6a
kymjaehong <dwby.jr@gmail.com>
2026-01-14 00:53:54 +0900

    fix: css & 편의기능(레퍼런스를 통한 ui 변화, 포커싱 등)

--- onebite-log/src/components/modal/post-editor-modal.tsx
+++ onebite-log/src/components/modal/post-editor-modal.tsx
@@ -2,24 +2,53 @@ import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { ImageIcon } from "lucide-react";
 import { usePostEditorModal } from "@/store/post-editor-modal";
+import { useEffect, useRef, useState } from "react";
 
 export default function PostEditorModal() {
   const { isOpen, close } = usePostEditorModal();
+
+  const [content, setContent] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   const handleCloseModal = () => {
     close();
   };
 
+  // 편의기능
+  // 1. 사용자가 textarea에 값을 입력할 때마다 Ref 객체를 이용해서 높이 수정
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height =
+        textareaRef.current.scrollHeight + "px";
+    }
+  }, [content]);
+
+  // 2. textarea에 포커싱
+  useEffect(() => {
+    if (!isOpen) return;
+    textareaRef.current?.focus();
+
+    // 3. 모달 닫히면 작성 중이던 내용 초기화
+    setContent("");
+  }, [isOpen]);
+
   return (
     <Dialog open={isOpen} onOpenChange={handleCloseModal}>
-      <DialogContent>
+      <DialogContent className="max-h-[90vh]">
         <DialogTitle>포스트 작성</DialogTitle>
-        <textarea />
-        <Button>
+        <textarea
+          ref={textareaRef}
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className="max-h-125 min-h-25 focus:outline-none"
+          placeholder="무슨 일이 있었나요?"
+        />
+        <Button variant={"outline"} className="cursor-pointer">
           이미지 추가
           <ImageIcon />
         </Button>
-        <Button>저장</Button>
+        <Button className="cursor-pointer">저장</Button>
       </DialogContent>
     </Dialog>
   );

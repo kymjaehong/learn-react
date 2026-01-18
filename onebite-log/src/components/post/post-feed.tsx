45ba4acc28b8023ed4e9ae849e20d1e4252ce3d8
kymjaehong <dwby.jr@gmail.com>
2026-01-19 00:03:29 +0900

    feat: 무한스크롤 기능 추가

--- onebite-log/src/components/post/post-feed.tsx
+++ onebite-log/src/components/post/post-feed.tsx
@@ -1,19 +1,32 @@
-import { usePostsData } from "@/hooks/queries/use-posts-data";
 import Fallback from "@/components/fallback";
 import Loader from "@/components/loader";
-import PostItem from "./post-item";
+import PostItem from "@/components/post/post-item";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { useInfinitePostsData } from "@/hooks/queries/use-infinite-posts-data";
 
 export default function PostFeed() {
-  const { data, error, isPending } = usePostsData();
+  const { data, error, isPending, fetchNextPage, isFetchingNextPage } =
+    useInfinitePostsData();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView]);
 
   if (error) return <Fallback />;
   if (isPending) return <Loader />;
 
   return (
     <div className="flex flex-col gap-10">
-      {data.map((post) => (
-        <PostItem key={post.id} {...post} />
-      ))}
+      {data?.pages.map((page) =>
+        page.map((post) => <PostItem key={post.id} {...post} />),
+      )}
+      {isFetchingNextPage && <Loader />}
+
+      <div ref={ref}></div>
     </div>
   );
 }

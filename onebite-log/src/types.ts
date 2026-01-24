9a93d7aff26d1b854ef4c96d21135f4c607ceb60
kymjaehong <dwby.jr@gmail.com>
2026-01-24 13:01:01 +0900

    feat: RPC를 활용한 좋아요 기능 추가

--- onebite-log/src/types.ts
+++ onebite-log/src/types.ts
@@ -2,6 +2,7 @@ import { type Database } from "./databse.types";
 
 export type PostEntity = Database["public"]["Tables"]["post"]["Row"];
 export type ProfileEntity = Database["public"]["Tables"]["profile"]["Row"];
+export type LikeEntity = Database["public"]["Tables"]["like"]["Row"];
 
 export type Post = PostEntity & { author: ProfileEntity };
 

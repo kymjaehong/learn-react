5e7eea98d500232ccef86be0d7431e949372350d
kymjaehong <dwby.jr@gmail.com>
2026-01-25 22:30:40 +0900

    fix: 좋아요 결과를 낙관적 업데이트로 반영

--- onebite-log/src/hooks/mutations/post/use-toggle-post-like.ts
+++ onebite-log/src/hooks/mutations/post/use-toggle-post-like.ts
@@ -1,14 +1,50 @@
 import { togglePostLike } from "@/api/post";
-import type { useMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import type { Post, useMutationCallback } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function useTogglePostLike(callbacks?: useMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: togglePostLike,
+    onMutate: async ({ postId }) => {
+      // 보통 좋아요 기능은 낙관적 업데이트로 버튼이 눌리자마자 캐시 데이터를 수정
+      await queryClient.cancelQueries({
+        queryKey: QUERY_KEYS.post.byId(postId),
+      });
+
+      // 업데이트 실패시 백업할 용도의 데이터
+      const prevPost = queryClient.getQueryData<Post>(
+        QUERY_KEYS.post.byId(postId),
+      );
+
+      // 낙관적 업데이트 실행
+      queryClient.setQueryData<Post>(QUERY_KEYS.post.byId(postId), (post) => {
+        if (!post) throw new Error("포스트가 존재하지 않습니다.");
+        return {
+          ...post,
+          isLiked: !post.isLiked,
+          like_count: post.isLiked ? post.like_count - 1 : post.like_count + 1,
+        };
+      });
+
+      // 실패시 원상 복구
+      // onError에서 context 매개변수로 제공
+      return {
+        prevPost,
+      };
+    },
     onSuccess: () => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
     },
-    onError: (error) => {
+    onError: (error, _, context) => {
+      if (context && context.prevPost) {
+        queryClient.setQueryData<Post>(
+          QUERY_KEYS.post.byId(context.prevPost.id),
+          context.prevPost,
+        );
+      }
       if (callbacks?.onError) callbacks.onError(error);
     },
   });

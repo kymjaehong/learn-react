6732f295ca5cd0e9e4f124a0505dab96b63bf825
kymjaehong <dwby.jr@gmail.com>
2026-01-27 23:57:01 +0900

    feat: 댓글 추가 기능 구현

--- onebite-log/src/types.ts
+++ onebite-log/src/types.ts
@@ -2,7 +2,7 @@ import { type Database } from "./databse.types";
 
 export type PostEntity = Database["public"]["Tables"]["post"]["Row"];
 export type ProfileEntity = Database["public"]["Tables"]["profile"]["Row"];
-export type LikeEntity = Database["public"]["Tables"]["like"]["Row"];
+export type CommentEntity = Database["public"]["Tables"]["comment"]["Row"];
 
 export type Post = PostEntity & { author: ProfileEntity; isLiked: boolean };
 

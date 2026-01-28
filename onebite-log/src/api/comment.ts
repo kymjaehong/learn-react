52b932cd56a4169c9cc471a77011213d46c00e75
kymjaehong <dwby.jr@gmail.com>
2026-01-29 00:00:04 +0900

    feat: 댓글 수정 기능 구현

--- onebite-log/src/api/comment.ts
+++ onebite-log/src/api/comment.ts
@@ -31,3 +31,23 @@ export async function fetchComments(postId: number) {
   if (error) throw error;
   return data;
 }
+
+export async function updateComment({
+  id,
+  content,
+}: {
+  id: number;
+  content: string;
+}) {
+  const { data, error } = await supabase
+    .from("comment")
+    .update({
+      content,
+    })
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) throw error;
+  return data;
+}
